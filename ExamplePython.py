d250b407699dfd82c7cb72fab3eb764c2a702072
nolan <nolanlanierdavila@gmail.com>
2025-11-13 15:01:53 -0800

    Update ExamplePython.py
    Added a Jobs list

--- ExamplePython.py
+++ ExamplePython.py
@@ -1,7 +1,17 @@
+#This is how we learned to concatenate strings in the ITF+ class.
 a = "String 1"
 b = "String 2"
 print("a =" + a + " and  b=" + b)
+
+#This syntax utilizes the concept of the f-string, which makes things a bit easier. No + sign needed!
 x = "String 3"
 y = "String 4"
 print(f"x = {x} and y = {y}")
+
+#We could also use an f-string like this:
 print("c = {c} and d = {d}".format(c=1, d=2))
+
+job_list = {'John':'Doctor', 'Jane': 'Engineer', 'Jim': 'Teacher'}
+
+for name, job in job_list.items():
+    print(f"{name} is a {job}")
